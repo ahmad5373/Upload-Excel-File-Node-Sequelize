382a3cd801e980599a0b0926e383f5d945a8cb25
Ahmed Raza <ahmadraxa550@gmail.com>
2022-06-16 00:34:38 -0700

    Format text

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
 const express = require("express");
 const app = express();
-const database = require('./database/connection');
+const database = require("./database/connection");
 global.__basedir = __dirname + "/.";
 const routes = require("./routes/upload");
-routes(app)
+routes(app);
 const port = process.env.port || 9000;
 
-app.get("/",  (req, res) => {
+app.get("/", (req, res) => {
   res.send("App is listening on port 9000");
 });
 
-
 //Use express.json as a middlewere
 app.use(express.json());
-
-
 
 app.listen(port, () => {
   console.log(`app is listening on : http://localhost:${port}`);
